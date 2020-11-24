ebc7f6850e7f254ba6ea60e213f2c0f769c6d0bd
LogarithmicEagle <51352080+LogarithmicEagle@users.noreply.github.com>
2020-11-24 09:23:18 -0600

    Added select-other-full key bind

--- key-bind.el
+++ key-bind.el
@@ -1,1 +1,10 @@
-(global-set-key "\C-x\C-b" 'buffer-menu-other-window)
+;; Make new window for listing of buffers
+(global-set-key (kbd "C-x C-b") 'buffer-menu-other-window)
+
+;; Make C-x C-o choose other window and make it full screem
+(defun select-other-full ()
+  (interactive)
+  (other-window 1)
+  (delete-other-windows))
+
+(global-set-key (kbd "C-x C-o") 'select-other-full)
