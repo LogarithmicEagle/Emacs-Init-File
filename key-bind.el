a318fcc8e97d11af945299e0f9709c795da74972
LogarithmicEagle <51352080+LogarithmicEagle@users.noreply.github.com>
2021-01-18 21:36:29 -0600

    Update key-bind.el

--- key-bind.el
+++ key-bind.el
@@ -1,5 +1,5 @@
-;; Make new window for listing of buffers
-(global-set-key (kbd "C-x C-b") 'buffer-menu-other-window)
+;; Make other buffer selected
+(global-set-key "\C-x\C-b" 'buffer-menu-other-window)
 
 ;; Make C-x C-o choose other window and make it full screem
 (defun select-other-full ()
@@ -8,3 +8,21 @@
   (delete-other-windows))
 
 (global-set-key (kbd "C-x C-o") 'select-other-full)
+
+;; Put copy as C-w
+(global-set-key (kbd "C-w") 'kill-ring-save)
+(global-set-key (kbd "M-w") 'kill-region)
+
+;; Swap a and enter in dired
+(eval-after-load "dired"
+  '(progn    
+     (define-key dired-mode-map (kbd "<RET>") 'dired-find-alternate-file)
+     (define-key dired-mode-map "a" 'dired-find-file)))
+
+;; Spell correct right click
+(eval-after-load "flyspell"
+  '(progn
+     (define-key flyspell-mouse-map [mouse-3] 'flyspell-correct-word)))
+
+;; Hyperspace
+(global-set-key (kbd "C-c C-h") 'hyperspace)
